bc672d46673d3e995e199bfa254e375646173174
malhotrah <hitanshumalhotra@gmail.com>
2014-11-20 20:35:59 +0530

    First Commit

---
+++ wp-content/themes/kallyas/admin/js/zn-ajax-options.js
@@ -0,0 +1,54 @@
+jQuery.noConflict();
+	jQuery(document).ready(function($){
+	
+	//hide hidden section on page load.
+	jQuery('#section-body_bg, #section-body_bg_custom, #section-body_bg_properties').hide();
+	
+
+	//hides warning if js is enabled			
+	$('#js-warning').hide();
+	
+	
+	
+	
+	/* save everything */
+	$('.zn_save').live('click',function() {
+			
+		var nonce = $('#security').val();
+					
+		$('.ajax-loading-img').fadeIn();
+										
+		var serializedReturn = $('#zn_form :input[name][name!="security"][name!="of_reset"]').serialize();
+										
+		//alert(serializedReturn);
+						
+		var data = {
+			type: 'save',
+			action: 'zn_ajax_post_action',
+			security: nonce,
+			data: serializedReturn
+		};
+					
+		$.post(ajaxurl, data, function(response) {
+			var success = $('.zn-save-popup');
+			var fail = $('.zn-fail-popup');
+			var loading = $('.ajax-loading-img');
+			loading.fadeOut();  
+						
+			if (response==1) {
+				success.fadeIn();
+			} else { 
+				fail.fadeIn();
+			}
+						
+			window.setTimeout(function(){
+				success.fadeOut(); 
+				fail.fadeOut();				
+			}, 2000);
+		});
+			
+	return false; 
+					
+	});   
+
+}); //end doc ready
---
+++ wp-content/themes/kallyas/template-helpers/contact_form.php
@@ -0,0 +1,141 @@
+<?php
+
+// Submit form
+function submit_form( $options ) {
+
+}
+
+
+// Text field
+function text ( $field ) {
+
+	$required = '';
+	$req = '';
+	$email_field = '';
+	
+	if ( $field['zn_cf_required'] ) {
+	
+		$required = 'zn_required_field';
+		
+		if (check_field ( $field['zn_cf_name'] ) ) {
+			
+			$req = 'error';
+		}
+		
+	}
+	
+	if ( $field['zn_cf_f_email'] ) {
+		$email_field = 'zn_email_field';
+	}
+
+	$field_name = preg_replace('~[\W\s]~', '_', $field['zn_cf_name']);
+	
+	 
+	echo '<div class="control-group '.$req.'">';
+		echo '<label class="control-label" for="'.$field_name.'">'.$field['zn_cf_name'].'</label>';
+		echo '<div class="controls">';
+			echo '<input id="'.$field_name.'" type="text" name="'.$field_name.'" placeholder="'.$field['zn_cf_name'].'" class="input-xlarge '.$required.' '.$email_field.'" title="'.$field['zn_cf_name'].'" />';
+		echo '</div>';
+	echo '</div><!-- end control group -->';
+}
+
+// Textarea field
+function textarea ( $field ) {
+
+
+	$required = '';
+	$req = '';
+	$email_field = '';
+
+	if ( $field['zn_cf_required'] ) {
+	
+		$required = 'zn_required_field';
+		
+		if (check_field ( $field['zn_cf_name'] ) ) {
+			
+			$req = 'error';
+		}
+		
+	}
+	
+	
+	if ( $field['zn_cf_f_email'] == 'yes' ) {
+		$email_field = 'zn_email_field';
+	}
+
+	$field_name = preg_replace('~[\W\s]~', '_', $field['zn_cf_name']);
+	
+	echo '<div class="control-group '.$req.'">';
+		echo '<label class="control-label" for="'.$field_name.'">'.$field['zn_cf_name'].'</label>';
+		echo '<div class="controls">';
+			echo '<textarea id="'.$field_name.'" name="'.$field_name.'" placeholder="'.$field['zn_cf_name'].'" class=" textarea span4 '.$required.' '.$email_field.'" title="'.$field['zn_cf_name'].'" ></textarea> ';
+		echo '</div>';
+	echo '</div><!-- end control group -->';
+}
+
+function check_field ( $field ) {
+
+	if ( isset ($_POST['cform_submit']) ) {
+		if ( isset ($_POST[$field]) && !empty ( $_POST[$field] ) ) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+}
+
+$element_size = zn_get_size( $options['_sizer'] );
+?>
+ 
+
+		<div class="<?php echo $element_size['sizer']; ?>">
+						
+			<?php if ( isset ( $options['zn_cf_desc'] ) && !empty ( $options['zn_cf_desc'] ) ) {
+				echo '<p>'.do_shortcode($options['zn_cf_desc']).'</p>';
+			}?>
+			
+			<div id="contact_form" class="rapid_contact ">
+			
+			<form  method="post" class="cf_validate form-horizontal zn_form">
+				<div id="success"></div>
+					<?php submit_form($options);?>
+					<?php 
+						if ( isset ( $options['zn_cf_fields'] ) ) {
+							foreach ( $options['zn_cf_fields'] as $field ) {
+								if (  function_exists( $field['zn_cf_type'] ) ) {
+									
+									
+										$field['zn_cf_type']($field);
+										
+									
+								}
+							}
+						}
+					?>
+				
+					<input type="hidden" name="cform_submit" value="cf_submitted" />
+					
+						<div class="control-group">
+							<div class="controls">
+								<input class=" btn " id="submit-form" type="submit" name="submit" value="<?php echo $options['zn_cf_button_value'];?>" />
+							</div>
+						</div><!-- end control group -->
+<div class="fb-comments" data-href="https://www.facebook.com/pages/Fitcomb-Functional-Intense-Tactical-Combat/166951149384" data-numposts="5" data-colorscheme="dark"></div>
+				</ul>
+			</form> 
+
+			</div>
+			
+		</div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=383619575089502&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+		
+<?php  ?>
